605fd565bf6ee6533298f0f69d62e2313a42bc2d
Patrick Barta <patrickbarta@patrickbarta.com>
2015-09-08 09:23:57 -0400

    Changed datetime from POSIXlt to POSIXct.

--- plot2.R
+++ plot2.R
@@ -2,8 +2,9 @@
 require(dplyr)
 
 # character constants
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 zip_file <- "household_power_consumption.zip"
-txt_file <- "household_power_consumption.txt"
+txt_file <- "household_power_consumption.txt" 
 dfSubsetFile <- "dfSubset.RData"
 
 # if subsetted data frame file exists, read it in, otherwise create it
@@ -11,33 +12,26 @@ if (file.exists(dfSubsetFile)) {
     dfSubset <- dget(dfSubsetFile)
 } else {
     # download file
-    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-                  "household_power_consumption.zip", method = "curl")
+    download.file(url, zip_file, method = "curl")
 
     #unzip file
-    unzip("household_power_consumption.zip")
+    unzip(zip_file)
     
     # read in data
-    dfSubset <- read.table(txt_file, header=TRUE, sep=";",
+    df <- read.table(txt_file, header=TRUE, sep=";",
                  colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"),
                  na.strings="?")
     
-    # reformat dates and times
-    #dfSubset$Date <- strptime(df$Date, format = "%d/%m/%Y")
-    
     # clean up df and subset it
     # filter gets rid of all the dates but 2007-02-01 and 2007-02-02 (note Date is given as day/month/year )
-    # mutate turns Date and Time together into a string parseable by srptime 
+    # mutate turns Date and Time together into a POSIXct object
     # select gets rid of Date/Time columns
-    dfSubset <- dfSubset %>% 
+    dfSubset <- df %>% 
                     filter(Date == "1/2/2007" | Date == "2/2/2007") %>%
-                    mutate(DateAndTime = paste(Date, Time, sep=" ")) %>%
+                    mutate(datetime = as.POSIXct(paste(Date, Time, sep=" "), format="%d/%m/%Y %H:%M:%S")) %>%
                     select(-(Date:Time))
 
-    # now, turn DateAndTime into POSIXlt (dplyr mutate can't handle POSIXlt)                      
-    dfSubset$DateAndTime <- strptime(dfSubset$DateAndTime, format="%d/%m/%Y %H:%M:%S")
-    
-    # write out subsetted data fram
+    # write out subsetted data frame
     dput(dfSubset, file=dfSubsetFile)
 }
 
@@ -46,7 +40,7 @@ if (file.exists(dfSubsetFile)) {
 png("plot2.png", bg="transparent")
 
 # do plot
-with(dfSubset, plot(DateAndTime, Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l"))
+with(dfSubset, plot(datetime, Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l"))
 
 #close png device
 dev.off()
--- plot4.R
+++ plot4.R
@@ -2,8 +2,9 @@
 require(dplyr)
 
 # character constants
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 zip_file <- "household_power_consumption.zip"
-txt_file <- "household_power_consumption.txt"
+txt_file <- "household_power_consumption.txt" 
 dfSubsetFile <- "dfSubset.RData"
 
 # if subsetted data frame file exists, read it in, otherwise create it
@@ -11,37 +12,29 @@ if (file.exists(dfSubsetFile)) {
     dfSubset <- dget(dfSubsetFile)
 } else {
     # download file
-    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-                  "household_power_consumption.zip", method = "curl")
+    download.file(url, zip_file, method = "curl")
 
     #unzip file
-    unzip("household_power_consumption.zip")
+    unzip(zip_file)
     
     # read in data
-    dfSubset <- read.table(txt_file, header=TRUE, sep=";",
+    df <- read.table(txt_file, header=TRUE, sep=";",
                  colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"),
                  na.strings="?")
     
-    # reformat dates and times
-    #dfSubset$Date <- strptime(df$Date, format = "%d/%m/%Y")
-    
     # clean up df and subset it
     # filter gets rid of all the dates but 2007-02-01 and 2007-02-02 (note Date is given as day/month/year )
-    # mutate turns Date and Time together into a string parseable by srptime 
+    # mutate turns Date and Time together into a POSIXct object
     # select gets rid of Date/Time columns
-    dfSubset <- dfSubset %>% 
+    dfSubset <- df %>% 
                     filter(Date == "1/2/2007" | Date == "2/2/2007") %>%
-                    mutate(DateAndTime = paste(Date, Time, sep=" ")) %>%
+                    mutate(datetime = as.POSIXct(paste(Date, Time, sep=" "), format="%d/%m/%Y %H:%M:%S")) %>%
                     select(-(Date:Time))
 
-    # now, turn DateAndTime into POSIXlt (dplyr mutate can't handle POSIXlt)                      
-    dfSubset$DateAndTime <- strptime(dfSubset$DateAndTime, format="%d/%m/%Y %H:%M:%S")
-    
-    # write out subsetted data fram
+    # write out subsetted data frame
     dput(dfSubset, file=dfSubsetFile)
 }
 
-# make the plot
 # open png device
 png("plot4.png", bg="transparent")
 
@@ -49,19 +42,19 @@ png("plot4.png", bg="transparent")
 par(mfcol=c(2,2))
 
 # do plot 1
-with(dfSubset, plot(DateAndTime, Global_active_power, xlab="", ylab="Global Active power", type="l"))
+with(dfSubset, plot(datetime, Global_active_power, xlab="", ylab="Global Active power", type="l"))
 
 # do plot 2
-with(dfSubset, plot(DateAndTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
-with(dfSubset, lines(DateAndTime, Sub_metering_2, col="red"))
-with(dfSubset, lines(DateAndTime, Sub_metering_3, col="blue"))
+with(dfSubset, plot(datetime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+with(dfSubset, lines(datetime, Sub_metering_2, col="red"))
+with(dfSubset, lines(datetime, Sub_metering_3, col="blue"))
 legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, bty="n")
 
 # do plot 3
-with(dfSubset, plot(DateAndTime, Voltage, type="l", xlab="datetime"))
+with(dfSubset, plot(datetime, Voltage, type="l"))
 
 # do plot 4
-with(dfSubset, plot(DateAndTime, Global_reactive_power, type="l", xlab="datetime"))
+with(dfSubset, plot(datetime, Global_reactive_power, type="l"))
 
 #close png device
 dev.off()
